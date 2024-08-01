ab98777560df0988872d4f09de54f0f8a6b9a208
Zekeriya <Zekeriya@ZEKERIYA>
2024-08-01 10:38:53 +0300

    Add project files.

---
+++ PratikConstructor/Bebek.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PratikConstructor
+{
+    
+    public class Bebek
+    {
+       public string name {  get; set; }
+       public string surname {  get; set; }
+       public DateTime birthDate { get; set; }
+        //default olan constructor
+        public Bebek()
+        {
+            birthDate = DateTime.Now;
+            name = "Bilinmeyen ad";
+            surname = "Bilinmeyen Soyad";
+            Console.WriteLine("Ingaaaaaaaaaaaa");
+        }
+        //Parametre alan constructor
+       public Bebek(string _name, string _surname)
+        {
+            name = _name;
+            surname = _surname;
+            birthDate = DateTime.Now;
+            Console.WriteLine("Ingaaaaaaaaaaaa");
+        }
+    }
+}
---
+++ PratikConstructor/Program.cs
@@ -0,0 +1,8 @@
+﻿using PratikConstructor;
+
+Bebek bebek1 = new Bebek();
+Console.WriteLine($"Bebeğin adı {bebek1.name}, Bebeğin soyadı {bebek1.surname}, Bebeğin doğum tarihi {bebek1.birthDate}");
+
+//parametre gönderiliyor
+Bebek bebek2 = new Bebek("Rick","Grimes");
+Console.WriteLine($"Bebeğin adı {bebek2.name}, Bebeğin soyadı {bebek2.surname}, Bebeğin doğum tarihi {bebek2.birthDate}");
